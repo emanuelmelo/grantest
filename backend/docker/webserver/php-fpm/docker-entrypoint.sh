b966403e7d6e3b8531689f34877ba1cab2af5f8f
emanuel.filho.ext Emanuel Pereira de Melo Filho <emanuel.filho.ext@infracommerce.com.br>
2022-02-25 02:40:26 -0300

    alteração da tecnologia de backend

--- backend/docker/webserver/php-fpm/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-if ! [ -d "./vendor" ]; then
-    composer install --ignore-platform-reqs  --no-interaction --verbose --no-suggest
-    php artisan migrate:refresh --seed
-fi
-exec "$@"
-set -- php-fpm
-exec "$@"
